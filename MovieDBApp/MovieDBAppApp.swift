2da238cee9017a3e7703ccf5c732282208a54ac7
ameen4455 <ameen4455@gmail.com>
2025-06-21 09:21:11 +0530

    init project

---
+++ MovieDBApp/MovieDBAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDBAppApp.swift
+//  MovieDBApp
+//
+//  Created by Ameen Azeez on 21/06/25.
+//
+
+import SwiftUI
+
+@main
+struct MovieDBAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
