27f4e78b507a20658045d98c012401f0eb6f4bb2
ameen4455 <ameen4455@gmail.com>
2025-06-23 16:19:31 +0530

    fix: added retries for ssl error

--- MovieDBApp/API/APIManager.swift
+++ MovieDBApp/API/APIManager.swift
@@ -22,26 +22,36 @@ class APIManager: APIManagerProtocol {
         guard let urlRequest = apiRequest.makeURLRequest() else {
             throw NetworkError.invalidUrl
         }
-        
-        do {
-            let (data, response) = try await session.data(for: urlRequest)
 
-            guard let httpResponse = response as? HTTPURLResponse else {
-                throw NetworkError.networkError
+        let decoder = JSONDecoder()
+        let maxRetries = 10 // retry for SSL errors
+
+        for attempt in 1...maxRetries {
+            do {
+                let (data, response) = try await session.data(for: urlRequest)
+
+                guard let httpResponse = response as? HTTPURLResponse else {
+                    throw NetworkError.networkError
+                }
+
+                guard (200...299).contains(httpResponse.statusCode) else {
+                    throw NetworkError.networkError
+                }
+
+                return try decoder.decode(T.self, from: data)
+            } catch is DecodingError {
+                throw NetworkError.parsingError
+            } catch {
+                if attempt == maxRetries {
+                    throw NetworkError.networkError
+                }
+                
+                // half second delay before SSL retry
+                try await Task.sleep(nanoseconds: 500_000_000)
             }
-
-            guard (200...299).contains(httpResponse.statusCode) else {
-                throw NetworkError.networkError
-            }
-
-            let decoder = JSONDecoder()
-            return try decoder.decode(T.self, from: data)
-            
-        } catch is DecodingError {
-            throw NetworkError.parsingError
-        } catch {
-            throw NetworkError.networkError
         }
+
+        throw NetworkError.networkError
     }
 }
 
